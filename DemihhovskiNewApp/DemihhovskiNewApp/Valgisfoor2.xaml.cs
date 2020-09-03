81bafbb4559de53c5c5c832e31d607f08a72ef7c
opilane <opilane@HP-CZC6297B89>
2020-09-03 10:43:16 +0300

    Valgusfoor 2

--- DemihhovskiNewApp/DemihhovskiNewApp/Valgisfoor2.xaml.cs
+++ DemihhovskiNewApp/DemihhovskiNewApp/Valgisfoor2.xaml.cs
@@ -18,30 +18,61 @@ namespace DemihhovskiNewApp
             Label punane = new Label()
             {
                 Text="Punane", 
-                TextColor=Color.Red,
+                TextColor=Color.Black,
                 FontSize=30,
                 FontAttributes=FontAttributes.Bold
+            };
+            Frame pun = new Frame()
+            {
+                BackgroundColor = Color.Red,
+                Content = punane,
+                CornerRadius = 90,
+                Margin = new Thickness(80, 0 ,80 ,0) 
             };
             Label kollane = new Label()
             {
                 Text = "Kollane",
-                TextColor = Color.Yellow,
+                TextColor = Color.Black,
                 FontSize = 30,
                 FontAttributes = FontAttributes.Bold
+            };
+            Frame kol = new Frame()
+            {
+                BackgroundColor = Color.Yellow,
+                Content = kollane,
+                CornerRadius = 90,
+                Margin = new Thickness(80, 0, 80, 0)
             };
             Label roheline = new Label()
             {
                 Text = "Roheline",
-                TextColor = Color.Green,
+                TextColor = Color.Black,
                 FontSize = 30,
                 FontAttributes = FontAttributes.Bold
             };
+            Frame roh = new Frame()
+            {
+                BackgroundColor = Color.Green,
+                Content = roheline,
+                CornerRadius = 90,
+                Margin = new Thickness(80, 0, 80, 0)
+            };
             StackLayout stackLayout = new StackLayout()
             {
-                Children = { punane, kollane, roheline }
+                Children = { pun, kol, roh }
             };
-            stackLayout.Orientation = StackOrientation.Horizontal;
+            StackLayout stackLayout1 = new StackLayout()
+            {
+                
+            };
+            StackLayout stackLayout2 = new StackLayout()
+            {
+
+            };
+            //stackLayout.Orientation = StackOrientation.Horizontal;
             Content = stackLayout;
+            Content = stackLayout1;
+            Content = stackLayout2;
         }
     }
 }
