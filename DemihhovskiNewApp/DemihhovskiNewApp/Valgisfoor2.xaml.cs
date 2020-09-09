c4b08562e79567001ff421955f0146ea94696f0a
opilane <opilane@HP-CZC6297B89>
2020-09-09 15:51:47 +0300

    Valgusfoor2 töötab

--- DemihhovskiNewApp/DemihhovskiNewApp/Valgisfoor2.xaml.cs
+++ DemihhovskiNewApp/DemihhovskiNewApp/Valgisfoor2.xaml.cs
@@ -12,67 +12,121 @@ namespace DemihhovskiNewApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Valgisfoor2 : ContentPage
     {
+        Label punane, kollane, roheline;
+        Frame pun, kol, roh;
+        Button sisse, valja;
+        bool sisse_valja;
         public Valgisfoor2()
         {
             //InitializeComponent();
-            Label punane = new Label()
+            punane = new Label()
             {
-                Text="Punane", 
-                TextColor=Color.Black,
-                FontSize=30,
-                FontAttributes=FontAttributes.Bold
+                Text="Punane",
+                HorizontalOptions = LayoutOptions.Center,
+                TextColor = Color.Black,
+                FontSize = 15,
+                FontAttributes = FontAttributes.Bold
             };
-            Frame pun = new Frame()
+            pun = new Frame()
             {
-                BackgroundColor = Color.Red,
+                BackgroundColor = Color.FromRgb(255, 0, 0),
                 Content = punane,
-                CornerRadius = 90,
-                Margin = new Thickness(80, 0 ,80 ,0) 
+                Padding = 80,
+                CornerRadius = 120,
+                Margin = new Thickness(100, 0 ,100 ,0) 
             };
-            Label kollane = new Label()
+            kollane = new Label()
             {
                 Text = "Kollane",
+                HorizontalOptions = LayoutOptions.Center,
                 TextColor = Color.Black,
-                FontSize = 30,
+                FontSize = 15,
                 FontAttributes = FontAttributes.Bold
             };
-            Frame kol = new Frame()
+            kol = new Frame()
             {
-                BackgroundColor = Color.Yellow,
+                BackgroundColor = Color.FromRgb(255, 255, 0),
                 Content = kollane,
-                CornerRadius = 90,
-                Margin = new Thickness(80, 0, 80, 0)
+                Padding = 80,
+                CornerRadius = 120,
+                Margin = new Thickness(100, 0, 100, 0)
             };
-            Label roheline = new Label()
+            roheline = new Label()
             {
                 Text = "Roheline",
+                HorizontalOptions = LayoutOptions.Center,
                 TextColor = Color.Black,
-                FontSize = 30,
+                FontSize = 13,
                 FontAttributes = FontAttributes.Bold
             };
-            Frame roh = new Frame()
+            roh = new Frame()
             {
-                BackgroundColor = Color.Green,
+                BackgroundColor = Color.FromRgb(0, 255, 0),
                 Content = roheline,
-                CornerRadius = 90,
-                Margin = new Thickness(80, 0, 80, 0)
+                Padding = 80,
+                CornerRadius = 120,
+                Margin = new Thickness(100, 0, 100, 0)
+            };
+            sisse = new Button()
+            {
+                Text = "sisse",
+                TextColor=Color.Black,
+                FontSize = 20,
+                BorderColor = Color.Gray,
+                BorderWidth = 2,
+                BackgroundColor =Color.LightGray,
+                Margin = new Thickness(120, 0, 120, 0)
+            };
+            valja = new Button()
+            {
+                Text = "valja",
+                TextColor = Color.Black,
+                FontSize = 20,
+                BorderColor = Color.Gray,
+                BorderWidth = 2,
+                BackgroundColor = Color.LightGray,
+                Margin = new Thickness(120, 0, 120, 0)
             };
             StackLayout stackLayout = new StackLayout()
             {
-                Children = { pun, kol, roh }
+                Children = { pun, kol, roh, sisse, valja }
             };
-            StackLayout stackLayout1 = new StackLayout()
-            {
-                
-            };
-            StackLayout stackLayout2 = new StackLayout()
-            {
-
-            };
+            sisse.Clicked += Sisse_Clicked;
+            valja.Clicked += Valja_Clicked;
             //stackLayout.Orientation = StackOrientation.Horizontal;
             Content = stackLayout;
-            Content = stackLayout1;
-            Content = stackLayout2;
+            var tap = new TapGestureRecognizer();
+            tap.Tapped += Tap_Tapped;
+            pun.GestureRecognizers.Add(tap);
+            kol.GestureRecognizers.Add(tap);
+            roh.GestureRecognizers.Add(tap);
+        }
+
+        private void Tap_Tapped(object sender, EventArgs e)
+        {
+            Frame fr=sender as Frame;
+            if (fr == pun & sisse_valja == false) { punane.Text = "Stopp"; }
+            else if (fr == kol & sisse_valja == false) { kollane.Text = "Oota"; }
+            else if (fr == roh & sisse_valja == false) { roheline.Text = "Mine"; }
+        }
+
+        private void Valja_Clicked(object sender, EventArgs e)
+        {
+            pun.BackgroundColor = Color.FromHex("#5A0000");
+            kol.BackgroundColor = Color.FromHex("#3F3F00");
+            roh.BackgroundColor = Color.FromHex("#004100");
+            punane.Text = "Punane";
+            kollane.Text = "Kollane";
+            roheline.Text = "Roheline";
+            sisse_valja = true;
+        }
+
+        private void Sisse_Clicked(object sender, EventArgs e)
+        {
+            pun.BackgroundColor = Color.FromRgb(255, 0, 0);
+            kol.BackgroundColor = Color.FromRgb(255, 255, 0);
+            roh.BackgroundColor = Color.FromRgb(0, 255, 0);
+            sisse_valja = false;
         }
     }
 }
